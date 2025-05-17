aac184a664f5d672196c8a398a258a42d67dadbb
rafaelsaored/AT2-IMC <rafaelfcfc@gmail.com>
2025-05-17 00:00:35 -0300

    Add files via upload

---
+++ ARQUIVO DICIONÁRIOS.py
@@ -0,0 +1,10 @@
+# Criação e inicialização do dicionário
+alunos = {"Ana": 8.5, "Pedro": 7.0, "Maria": 9.2}
+alunos["Carlos"] = 6.5
+alunos["Ana"] = 9.0
+alunos.pop("Pedro")
+media = sum(alunos.values()) / len(alunos)
+if "Maria" in alunos:
+    print("Maria está no dicionário.")
+else:
+    print("Maria não está no dicionário.")
