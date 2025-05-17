e83c95361b66c2d44745be2cbf7e7852a4bad0a8
rafaelsaored/AT2-IMC <rafaelfcfc@gmail.com>
2025-05-17 00:03:23 -0300

    Add files via upload

--- ARQUIVO LISTAS.py
+++ ARQUIVO LISTAS.py
@@ -1,18 +1,8 @@
 animais = ['cachorro', 'gato', 'papagaio']
 
-# Adiciona 'hamster' ao final
 animais.append('hamster')
-
-# Insere 'coelho' na posição 1
 animais.insert(1, 'coelho')
-
-# Remove o último animal da lista
 animais.pop()
-
-# Remove o animal da posição 2
 animais.pop(2)
-
-# Altera o animal da posição 0 para 'leão'
 animais[0] = 'leão'
-
 print(animais)
